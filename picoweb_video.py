b88a0c80a9921c9f23a3127d7d8e2e305228e84e
s0578026 <kay.birth@student.htw-berlin.de>
2023-01-15 13:44:54 +0100

    removed unnecessary comments

--- picoweb_video.py
+++ picoweb_video.py
@@ -1,44 +1,33 @@
-# This section uses firmware from Lemariva's Micropython-camera-driver.  
-# for details, please refer to: https://github.com/lemariva/micropython-camera-driver  
 import picoweb
 import utime
 import camera
 import gc
 import machine
 
-# Initializing the Camera
+
 def camera_init():
-    # Disable camera initialization
     camera.deinit()
-    # Enable camera initialization
     camera.init(0, d0=4, d1=5, d2=18, d3=19, d4=36, d5=39, d6=34, d7=35,
                 format=camera.JPEG, framesize=camera.FRAME_VGA, 
                 xclk_freq=camera.XCLK_20MHz,
                 href=23, vsync=25, reset=-1, pwdn=-1,
                 sioc=27, siod=26, xclk=21, pclk=22, fb_location=camera.PSRAM)
 
-    camera.framesize(camera.FRAME_VGA) # Set the camera resolution
+    camera.framesize(camera.FRAME_VGA)
     # The options are the following:
     # FRAME_96X96 FRAME_QQVGA FRAME_QCIF FRAME_HQVGA FRAME_240X240
     # FRAME_QVGA FRAME_CIF FRAME_HVGA FRAME_VGA FRAME_SVGA
     # FRAME_XGA FRAME_HD FRAME_SXGA FRAME_UXGA
-    # Note: The higher the resolution, the more memory is used.
-    # Note: And too much memory may cause the program to fail.
     
     camera.flip(1)                       # Flip up and down window: 0-1
     camera.mirror(1)                     # Flip window left and right: 0-1
     camera.saturation(0)                 # saturation: -2,2 (default 0). -2 grayscale 
     camera.brightness(0)                 # brightness: -2,2 (default 0). 2 brightness
     camera.contrast(0)                   # contrast: -2,2 (default 0). 2 highcontrast
-    camera.quality(10)                   # quality: # 10-63 lower number means higher quality
-    # Note: The smaller the number, the sharper the image. The larger the number, the more blurry the image
-    
-    camera.speffect(camera.EFFECT_NONE)  # special effects:
-    # EFFECT_NONE (default) EFFECT_NEG EFFECT_BW EFFECT_RED EFFECT_GREEN EFFECT_BLUE EFFECT_RETRO
-    camera.whitebalance(camera.WB_NONE)  # white balance
-    # WB_NONE (default) WB_SUNNY WB_CLOUDY WB_OFFICE WB_HOME
+    camera.quality(10)                   # quality: # 10-63 lower number means higher quality    
+    camera.speffect(camera.EFFECT_NONE)
+    camera.whitebalance(camera.WB_NONE)
 
-# HTTP Response Content  
 index_web="""
 HTTP/1.0 200 OK\r\n
 <html>
@@ -54,10 +43,8 @@ HTTP/1.0 200 OK\r\n
 
 # HTTP Response
 def index(req, resp):
-    # You can construct an HTTP response completely yourself, having
     yield from resp.awrite(index_web)
 
-# Send camera pictures
 def send_frame():
     buf = camera.capture()
     yield (b'--frame\r\n'
@@ -66,16 +53,13 @@ def send_frame():
     del buf
     gc.collect()
         
-# Video transmission
 def video(req, resp):
     yield from picoweb.start_response(resp, content_type="multipart/x-mixed-replace; boundary=frame")
     while True:
         yield from resp.awrite(next(send_frame()))
         gc.collect()
 
-
 ROUTES = [
-    # You can specify exact URI string matches...
     ("/", index),
     ("/video", video),
 ]
